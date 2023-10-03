c72804224ca7d9c93a227709e7cfebfb89b41382
matador7495 <id4acc2014@gmail.com>
2023-10-03 20:48:26 +0330

    create  js file for navlist

---
+++ script.js
@@ -0,0 +1,9 @@
+let menu=document.querySelector("#menu-icon");
+let navlist=document.querySelector(".navlist");
+menu.addEventListener("click",()=>{
+    navlist.classList.toggle("active");
+
+});
+window.onscroll = () =>{
+    navlist.classList.remove("active");
+}
